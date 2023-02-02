c24bd2e22b2049243963a63155339f0aa31eb510
Vadym <vv5buz@gmail.com>
2023-02-02 22:13:23 +0200

    Add buttons to edit task and to delete it

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # This is my first JavaScript to-do list.
 
 ## How to use
-Type the text of your task in the field below. After you push the button, your task will be added to the task list. If you have finished your task, just click on it and the task will be marked as complete and will gone.
+Type the text of your task in the field below. After you push the button, your task will be added to the task list. If you have finished your task, just click at the 'delete' button againts your task. The task will be marked as complete and will gone. If you want to change the text of your task so click on 'edit' button againts your task then put the new text of task and click on 'Save' button.
--- todo.js
+++ todo.js
@@ -1,18 +1,31 @@
+const editTaskBtn = '<div class="task-btn edit-task-btn" onclick="editTask(this.parentElement)">edit</div>';
+const removeTaskBtn = '<div class="task-btn remove-task-btn" onclick="removeTask(this.parentElement)">delete</div>';
+
 function getNewTask() {
     if (document.getElementById('new-task-text').value === '') {
         alert('Fill the task text field');
     } else {
         document.getElementById('task-list').appendChild(document.createElement("li"));
-        document.getElementById('task-list').lastElementChild.innerText = document.getElementById('new-task-text').value;
-        document.getElementById('task-list').lastElementChild.setAttribute('onclick','removeTask(this)');
-        document.getElementById('task-list').lastElementChild.style.cursor = 'pointer';
+        document.getElementById('task-list').lastElementChild.innerHTML = document.getElementById('new-task-text').value + removeTaskBtn + editTaskBtn;
         document.getElementById('new-task-text').value = '';
     }
 }
 
 function removeTask(taskToDelete) {
-    taskToDelete.style.textDecoration = 'line-through';
+    taskToDelete.classList.add('completed-task');
     setTimeout(() => {
         taskToDelete.remove();
     }, 800);
+}
+
+function editTask(taskToEdit) {
+    const editField = '<input type="text" value="' + taskToEdit.innerText.slice(0, -11) + '">';
+    const saveEditBtn = '<button type="button" onclick="saveEditedText(this.parentElement)" style="margin-left: 5px;">Save</button>';
+    taskToEdit.innerHTML = '<div style="display:flex;">' + editField + saveEditBtn; + '</div>';
+}
+
+function saveEditedText (saveBtn) {
+    const currentTask = saveBtn.parentElement;
+    newTaskText = currentTask.querySelector('input').value;
+    currentTask.innerHTML = newTaskText + removeTaskBtn + editTaskBtn;
 }
